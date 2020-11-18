3950e7bd633ffb04ab42e046e58942e5c5bbf7d0
Gabrielle Madden <gabriellemadden@users.noreply.github.com>
2020-11-18 16:57:14 -0500

    Add spark helm release script since release ci isnt working

---
+++ scripts/release-chart.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+GITHUB_PAT=$1
+HEAD_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ -z "$GITHUB_PAT" ]; then
+    echo "Usage: ./release-chart.sh GITHUB_PAT"
+    exit 1
+fi
+
+check_dirty() {
+if [ -n "$(git status | grep modified)" ]; then
+    echo "Please clean your git status before continuing."
+    exit 1
+fi
+}
+
+package() {
+cr package charts/spark 
+}
+
+upload() {
+cr upload \
+    --owner GarnerCorp \
+    --git-repo business-analytics \
+    --token $GITHUB_PAT
+}
+
+update_index() {
+cr index \
+    --owner GarnerCorp \
+    --git-repo business-analytics \
+    --charts-repo https://garnercorp.github.io/business-analytics/ \
+    --token $GITHUB_PAT \
+    --index-path . 
+}
+
+release_to_ghpages() {
+git fetch
+git checkout gh-pages \
+&& update_index \
+&& git add index.yaml \
+&& git commit -m "Add index for new spark release" \
+&& git push \
+&& git checkout $HEAD_BRANCH
+}
+
+check_dirty
+rm -rf .cr-release-packages charts/spark/Chart.lock
+package && upload && release_to_ghpages
